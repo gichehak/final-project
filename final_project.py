8b6c39c49a075e2793928c8ad425127e70dcb5fa
Lisa Chen <lisachen@udel.edu>
2017-11-28 23:57:00 -0500

    database edit

--- final_project.py
+++ final_project.py
@@ -14,20 +14,19 @@ db = SQLAlchemy(app)
 
 
 # # define database tables
-# class Artist(db.Model):
+# class Customer(db.Model):
 #     __tablename__ = 'customers'
 #     id = db.Column(db.Integer, primary_key=True)
 #     name = db.Column(db.String(64))
-#     about = db.Column(db.Text)
-#     orders = db.relationship('Song', backref='artist')
+#     address = db.Column(db.Text)
+#     orders = db.relationship('Order', backref='customer')
 #
-# class Song(db.Model):
+# class Order(db.Model):
 #     __tablename__ = 'orders'
 #     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(256))
-#     year = db.Column(db.Integer)
-#     lyrics = db.Column(db.Text)
-#     artist_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+#     description = db.Column(db.String(256))
+#     order_date = db.Column(db.Integer)
+#     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
 
 @app.route('/')
 def index():
@@ -41,50 +40,50 @@ def show_all_customers():
     customers = Customer.query.all()
     return render_template('customer-all.html', customers=customers)
 
-# @app.route('/artist/add', methods=['GET', 'POST'])
+# @app.route('/customer/add', methods=['GET', 'POST'])
 # def add_customers():
 #     if request.method == 'GET':
-#         return render_template('artist-add.html')
+#         return render_template('customer-add.html')
 #     if request.method == 'POST':
 #         # get data from the form
 #         name = request.form['name']
 #         about = request.form['about']
 #
 #         # insert the data into the database
-#         artist = Artist(name=name, about=about)
-#         db.session.add(artist)
+#         customer = Customer(name=name, about=about)
+#         db.session.add(customer)
 #         db.session.commit()
 #         return redirect(url_for('show_all_customers'))
 
-    # @app.route('/artist/edit/<int:id>', methods=['GET', 'POST'])
-    # def edit_artist(id):
-    #     artist = Artist.query.filter_by(id=id).first()
+    # @app.route('/customer/edit/<int:id>', methods=['GET', 'POST'])
+    # def edit_customers(id):
+    #     customer = Customer.query.filter_by(id=id).first()
     #     if request.method == 'GET':
-    #         return render_template('artist-edit.html', artist=artist)
+    #         return render_template('customer-edit.html', customer=customer)
     #     if request.method == 'POST':
     #         # update data based on the form data
-    #         artist.name = request.form['name']
-    #         artist.about = request.form['about']
+    #         customer.name = request.form['name']
+    #         customer.about = request.form['about']
     #         # update the database
     #         db.session.commit()
     #         return redirect(url_for('show_all_customers'))
 
-# @app.route('/song/add', methods=['GET', 'POST'])
+# @app.route('/order/add', methods=['GET', 'POST'])
 # def add_orders():
 #     if request.method == 'GET':
-#         customers = Artist.query.all()
-#         return render_template('song-add.html', customers=customers)
+#         customers = Customer.query.all()
+#         return render_template('order-add.html', customers=customers)
 #     if request.method == 'POST':
 #         # get data from the form
 #         name = request.form['name']
 #         year = request.form['year']
 #         lyrics = request.form['lyrics']
-#         artist_name = request.form['artist']
-#         artist = Artist.query.filter_by(name=artist_name).first()
-#         song = Song(name=name, year=year, lyrics=lyrics, artist=artist)
+#         customer_name = request.form['customer_name']
+#         customer = Customer.query.filter_by(name=customer_name).first()
+#         order = Order(name=name, year=year, lyrics=lyrics, customer=customer)
 #
 #         # insert the data into the database
-#         db.session.add(song)
+#         db.session.add(order)
 #         db.session.commit()
 #         return redirect(url_for('show_all_orders'))
 
